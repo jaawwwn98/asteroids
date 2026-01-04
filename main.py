868565fb465e308d3298db89b00e2542f0b1180e
johnobong <jonbong98@gmail.com>
2026-01-04 16:24:55 +0000

    add forward and backwards movement

--- main.py
+++ main.py
@@ -14,16 +14,18 @@ def main():
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        clock.tick(60)
-        dt = clock.tick(60) / 1000
-        player.draw(screen)
+
         player.update(dt)
 
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
